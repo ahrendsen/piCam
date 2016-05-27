49ccf8a5498ffddead7c9880f9d474ffc1594c33
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2016-05-27 07:56:07 -0500

    Did a lot of work on the automatic bubble counting thing. Thinking about implementing it in the near future, but deciding I should probably go into work instead of doing it now. My main problem is going to be figuring out how to stop motion from running, because that was causing me a lot of issues, making the computer run really slowly for some reason.

---
+++ cam/motion/onEventEnd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This is the script to run when the event ends. It will
+# complete the following tasks:
+#
+#   1. Write the value contained in the counts file to
+#      another file, along with the date.
+#   2. Kill motion.
+#
+# It's unreasonable to write five of these scripts for each 
+# mask, so instead a variable will be input to the script
+# which represents the bottle to track.
+#
+#   Usage: onEventStart.sh <number representing the bottle>
+#                            (an integer between 0 and 9)
+#
+
+ENDINGFILE="/home/karl/piCam/cam/motion/bubbleCounts/ending"
+declare -i ENDING=$(cat $ENDINGFILE)
+if [[ $ENDING -eq 0 ]]; then
+    BUBBLECOUNTFILE="/home/karl/piCam/cam/motion/bubbleCounts/bubbleCount$1"
+    DATE=$(date +"%d_%m_%Y")
+    declare -i BUBBLECOUNT=$(cat $BUBBLECOUNTFILE)
+    echo "$DATE	$BUBBLECOUNT" >> "bubbleCounts/countRecords$1"
+    echo "1" > $ENDINGFILE
+    service motion stop
+fi
