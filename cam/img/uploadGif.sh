1651b1eea520b997602c82cddafe89730638420d
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-02-21 06:48:31 -0600

    Made the script more generic so that an argument can be given to upload a gif in any folder.

--- cam/img/uploadGif.sh
+++ cam/img/uploadGif.sh
@@ -2,18 +2,22 @@
 
 if [ $# -lt 1 ]; then
 	DATE='yesterday'
-	echo -e "\n\nNo argument supplied. Compiling yesterday's data\n\n"
+	echo -e "\n\nNo argument supplied. Uploading yesterday's data\n\n"
 	sleep 5
+
+	# Set the directory where your pictures are stored.
+	MONTH=$(date +"%m" --date=$DATE)
+	DAY=$(date +"%d" --date=$DATE)
+
+
+	GIFNAME="AutoGif_$MONTH-${DAY}_30fps.gif"
+	UPLOADFOLDER=$MONTH
 else
-	DATE=$1
+	GIFNAME=$1
+	UPLOADFOLDER=$2
 fi
 
-# Set the directory where your pictures are stored.
-MONTH=$(date +"%m" --date=$DATE)
-DAY=$(date +"%d" --date=$DATE)
-
 GIFDIR=/home/pi/AutoGif
-GIFNAME="AutoGif_$MONTH-${DAY}_30fps.gif"
 
 # The following lines will upload the gif to cloud storage.
-rclone copy $GIFDIR/$GIFNAME unlbox:piCamPics/$MONTH
+rclone copy $GIFDIR/$GIFNAME unlbox:piCamPics/$UPLOADFOLDER
