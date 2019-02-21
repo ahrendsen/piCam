9a350ff0e395eb360f62c2d8864159585b1ee76e
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-02-21 06:53:16 -0600

    NOW the gif compile script is more generic

--- cam/img/compileFilesIntoGif.sh
+++ cam/img/compileFilesIntoGif.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+#
+# Usage ./compileFilesIntoGif.sh <folder containing jpgs> <GIF filename> 
 
 # Before a video can be compiled, you must rename the files to be in an integer ascending order.
 # run the script renameFiles.sh before running this script.
-if [ $# -lt 1 ]; then
+if [ $# -lt 2]; then
 	DATE='yesterday'
 	echo -e "\n\nNo argument supplied. Compiling yesterday's data\n\n"
 	sleep 5
+
+	# Set the directory where your pictures are stored.
+	MONTH=$(date +"%m" --date=$DATE)
+	DAY=$(date +"%d" --date=$DATE)
+	DIR=$MONTH/$DAY
+
+	NAME="AutoGif_$MONTH-${DAY}_${FRAMERATE}fps.gif"
+
 else
-	DATE=$1
+	DIR=$1
+	NAME=$2
 fi
 
-# Set the directory where your pictures are stored.
-MONTH=$(date +"%m" --date=$DATE)
-DAY=$(date +"%d" --date=$DATE)
 
-PICDIR=/home/pi/RbPics/$MONTH/$DAY
+PICDIR=/home/pi/RbPics/$DIR
 # Directory where you want the video to be saved. Make sure it already exists. 
 GIFDIR=/home/pi/AutoGif
 mkdir -p "${GIFDIR}"
@@ -22,6 +30,6 @@ mkdir -p "${GIFDIR}"
 #VIDEO OPTIONS
 FRAMERATE=30
 # Set the name that you want your video to have. 
-GIFNAME="AutoGif_$MONTH-${DAY}_${FRAMERATE}fps.gif"
+GIFNAME=$NAME
 
-ffmpeg -framerate $FRAMERATE -pattern_type glob -i "${PICDIR}/renamed/*.jpg" -r 15 -vf scale=512:-1 ${GIFDIR}/$GIFNAME
+ffmpeg -framerate $FRAMERATE -pattern_type glob -i "${PICDIR}/*.jpg" -r 15 -vf scale=512:-1 ${GIFDIR}/$GIFNAME
