f8f252dcd74075e985fbac7050df4f12a38acc56
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-02-21 06:54:07 -0600

    Added script for auto compiling the last 12 hours and uploading to my personal website.

---
+++ cam/img/processRolling12Hours.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Renaming Files"
+/home/pi/picam/cam/img/renameFilesRolling12Hours.sh
+echo "Compiling Gif"
+yes | /home/pi/picam/cam/img/compileDirectoryIntoGif.sh rolling rolling.gif
+echo "Uploading Gif"
+/home/pi/picam/cam/img/uploadGif.sh rolling.gif 
+
+echo "Uploading Gif to ahrendsen.net"
+scp -P 7822 /home/pi/AutoGif/rolling.gif root@ahrendsen.net:/var/www/html/img
---
+++ cam/img/renameFilesRolling12Hours.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+
+DIR="/home/pi/RbPics/rolling"
+mkdir -p "$DIR"
+
+ITER=0
+
+# Set this to the directory containing the images to compile
+PICDIR=/home/pi/RbPics
+# Leave this blank if only using one camera
+CAMERAEXTENSION=""
+
+# An iteration variable.
+for f in $( ls ~/RbPics/*/*/*.jpg | tail -n 144 ); do 
+	NAME=$(printf "%05d" ${ITER})
+	sudo cp "${f}" "$DIR/${NAME}.jpg"
+	ITER=$((ITER+1)) 
+done
