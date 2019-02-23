8920415432ad12b4d74cb6a891f58bbb9624e5e9
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-02-23 16:37:57 -0600

    Minor edits to allow FPS to be in fileName

--- cam/img/compileFilesIntoGif.sh
+++ cam/img/compileFilesIntoGif.sh
@@ -4,7 +4,7 @@
 
 # Before a video can be compiled, you must rename the files to be in an integer ascending order.
 # run the script renameFiles.sh before running this script.
-if [ $# -lt 2]; then
+if [ $# -lt 2 ]; then
 	DATE='yesterday'
 	echo -e "\n\nNo argument supplied. Compiling yesterday's data\n\n"
 	sleep 5
@@ -12,23 +12,22 @@ if [ $# -lt 2]; then
 	# Set the directory where your pictures are stored.
 	MONTH=$(date +"%m" --date=$DATE)
 	DAY=$(date +"%d" --date=$DATE)
-	DIR=$MONTH/$DAY
+	DIR=/home/pi/RbPics/$MONTH/$DAY
 
+	#VIDEO OPTIONS
+	FRAMERATE=30
 	NAME="AutoGif_$MONTH-${DAY}_${FRAMERATE}fps.gif"
-
 else
 	DIR=$1
 	NAME=$2
 fi
 
 
-PICDIR=/home/pi/RbPics/$DIR
+PICDIR=$DIR
 # Directory where you want the video to be saved. Make sure it already exists. 
 GIFDIR=/home/pi/AutoGif
 mkdir -p "${GIFDIR}"
 
-#VIDEO OPTIONS
-FRAMERATE=30
 # Set the name that you want your video to have. 
 GIFNAME=$NAME
 
