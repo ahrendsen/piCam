33bdfa41d168fbf3d0e7c1632e8e2bbe34d6f679
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-02-23 16:49:50 -0600

    Changing to multiple rolling scales, updating processing to reflect that

--- cam/img/processRolling12Hours.sh
+++ cam/img/processRolling12Hours.sh
@@ -3,9 +3,9 @@
 echo "Renaming Files"
 /home/pi/picam/cam/img/renameFilesRolling12Hours.sh
 echo "Compiling Gif"
-yes | /home/pi/picam/cam/img/compileDirectoryIntoGif.sh rolling rolling.gif
+yes | /home/pi/picam/cam/img/compileDirectoryIntoGif.sh rolling12 rolling.gif
 echo "Uploading Gif"
-/home/pi/picam/cam/img/uploadGif.sh rolling.gif 
+/home/pi/picam/cam/img/uploadGif.sh rolling12.gif 
 
 echo "Uploading Gif to ahrendsen.net"
 scp -P 7822 /home/pi/AutoGif/rolling.gif root@ahrendsen.net:/var/www/html/img
