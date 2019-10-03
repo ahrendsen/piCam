fb47e5471ce0cfd4f7f6475d3ef9d9dceb8187a5
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-10-03 14:40:04 -0500

    Last upload from macbook, now using dell

---
+++ cam/motion/gphotoCapture.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#sudo rm $HOME/picam/cam/motion/stopCamera
+#while [ ! -f stopCamera ];
+#do 
+#	sudo gphoto2 --capture-image --frames 1 --interval 1
+#done 
---
+++ cam/motion/script.sh
@@ -0,0 +1,1 @@
+echo "EVENT HAPPENED!" >> eventLog.txt
